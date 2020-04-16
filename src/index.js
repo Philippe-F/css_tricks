70de087c677e470033fb5f8a98e23bd1f5639c71
Philippe Fonzin <pfonzin@gmail.com>
2020-04-16 11:49:09 -0400

    style nav bar

--- src/index.js
+++ src/index.js
@@ -1,11 +1,4 @@
-import _ from 'lodash';
 
-function component() {
-  const element = document.createElement('div');
-
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-  return element;
-}
-
-document.body.appendChild(component());
+document.addEventListener("DOMContentLoaded", () => {
+  document.getElementById("root");
+});
